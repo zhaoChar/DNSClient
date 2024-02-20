90dca788ee4dae3ec38377b661e704731f18f9ce
Alon Hillel-Tuch <ah5647@nyu.edu>
2024-02-20 18:51:21 -0500

    Update DNSClient.py

--- DNSClient.py
+++ DNSClient.py
@@ -6,7 +6,7 @@ real_name_server = ???? # Research public DNS servers to find a valid DNS server
 
 
 # Create a list of domain names to query - use the same list from the DNS Server
-domainList  = ['example.com.','safebank.com','google.com','nyu.edu','legitsite.com']
+domainList  = ['example.com.','safebank.com.','google.com.','nyu.edu.','legitsite.com.']
 
 # Define a function to query the local DNS server for the IP address of a given domain name
 def query_local_dns_server(domain,question_type):
