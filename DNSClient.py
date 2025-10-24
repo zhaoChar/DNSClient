60be060e4a6a424562ecdbca2c6fdee9b18ff969
zhaoChar <zhao.char@northeastern.edu>
2025-10-24 17:39:48 -0400

    needs to be 127.0.0.1 for gradescope

--- DNSClient.py
+++ DNSClient.py
@@ -1,7 +1,7 @@
 import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
-local_host_ip = '192.168.1.1'
+local_host_ip = '127.0.0.1'
 real_name_server = '1.1.1.1' # Research public DNS servers to find a valid DNS server IP address to use
 
 
